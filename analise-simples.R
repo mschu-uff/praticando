252c6986cbd2a99364049198e3c76d07205c546e
Matheus Schuindt <mschuindt@id.uff.br>
2026-03-30 10:16:20 -0300

    análise descritiva das variáveis qualitativas

--- analise-simples.R
+++ analise-simples.R
@@ -1,4 +1,30 @@
-# Análise simples
+# Funções auxiliares -----------------------------
 
-# Leitura dos dados
+analise_qualitativa <- function(x) {
+  n <- table(x)
+  f <- prop.table(n)
+  cbind(n, f)
+}
+
+# Leitura dos dados ------------------------
+
 df <- read.csv("dados - respostas formulário.csv")
+
+# Análise descritiva das variáveis qualitativas --------------------------
+
+for (coluna in c("ingresso", "sexo", "grupo_sanguineo", "ingere_pao",
+                 "ingere_massa", "ingere_refri", "ingere_alcool")) {
+  cat(coluna, "\n")
+  print(analise_qualitativa(df[,coluna]))
+  cat("\n")
+}
+
+frutas_flat = c()
+for (frutas in strsplit(df$fruta_gosta, ", ")) {
+  if (frutas[length(frutas)] == "Nenhuma das frutas mencionadas") {
+    frutas_flat <- c(frutas_flat, "Nenhuma")
+  } else {
+    frutas_flat <- c(frutas_flat, frutas)
+  }
+}
+sort(table(frutas_flat), decreasing=TRUE)
